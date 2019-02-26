e88bb847c6ca80c27b9bd2d04c5feee670b0c6e6
Stephanie Yao <stephyaospam@gmail.com>
2019-02-26 00:29:37 -0500

    Made progress on question 3. Almost done, just need to figure out y-axis ordering and distribution of points doesn't exactly match the answer.

--- script.R
+++ script.R
@@ -1,6 +1,7 @@
 library(readr)
 library(tidyverse)
 library(gt)
+library(ggstance)
 
 elections <- read_csv("ps_4_elections-poll-nc09-3.csv",
                       col_types = cols(
@@ -47,11 +48,10 @@ first_responses <- elections %>%
 # do we need to reinstall gt every time?
 # how should we deal with the rows where file_race does not match race_eth?
 
-elections %>%
+table <- elections %>%
   select(response, final_weight, file_race) %>%
   group_by(file_race, response) %>%
   summarize(total = sum(final_weight)) %>%
-  #filter(file_race != race_eth) %>%
   spread(key = response, value = total) %>%
   mutate(all = Dem + Rep + Und) %>% 
   mutate(Dem = Dem / all) %>% 
@@ -70,5 +70,22 @@ elections %>%
   fmt_percent(columns = vars(Dem, Rep, Und), decimals = 0) %>% 
   as_raw_html() %>% as.character() %>% cat()
   
-  
+# question 3
+elections %>%
+  select(educ, final_weight) %>%
+  filter(educ != "[DO NOT READ] Refused") %>%
+  within(elections, stage <- factor(stage, levels = c("High school", "Grade school",
+                                                      "Some college or trade school", 
+                                                      "Bachelors' degree",
+                                                      "Graduate or Professional Degree"))) %>%
+  ggplot(aes(x = final_weight, y = educ)) +
+  geom_violinh(fill = 'white') +
+  geom_jitter(alpha = 0.3) +
+  labs(title = "More Educated Matter Less in North Carolina 9th",
+       subtitle = "Poll gives more weight to people who are less likely to participate in polls",
+       caption = "New York Times Upshot/Siena College 2018 live polls") +
+  xlab("Weight Given to Respondent in Calculating Poll Results") +
+  ylab(NULL)
+
+# question 4
 
